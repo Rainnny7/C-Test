ef71ef6c154ae0cdc541c04dd6a623d896965fab
Braydon <xcoolmanxx@gmail.com>
2020-07-11 12:03:25 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,35 +11,37 @@ namespace Test
             DateTime before = DateTime.UtcNow;
 
             // Elapsed: 39.5ms
-            int num = 0;
-            for (int i = 0; i < 10000000; i++) {
-                num++;
-            }
+            // int num = 0;
+            // for (int i = 0; i < 10000000; i++) {
+            //     num++;
+            // }
 
             // Elapsed: 3331.3ms
-            string a = "";
-            for (int i = 0; i < 100000; i++) {
-                a+= "a";
-            }
+            // string a = "";
+            // for (int i = 0; i < 100000; i++) {
+            //     a+= "a";
+            // }
 
             // Elapsed: 1.2ms
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < 100000; i++) {
-                builder.Append("a");
-            }
+            // StringBuilder builder = new StringBuilder();
+            // for (int i = 0; i < 100000; i++) {
+            //     builder.Append("a");
+            // }
 
             // Elapsed: 118.5ms
-            string[] strings = new string[10000000];
-            for (int i = 0; i < 10000000; i++) {
-                strings[i] = "a";
-            }
+            // string[] strings = new string[10000000];
+            // for (int i = 0; i < 10000000; i++) {
+            //     strings[i] = "a";
+            // }
 
             // Elapsed: 299.4ms
-            ArrayList list = new ArrayList();
-            for (int i = 0; i < 10000000; i++) {
-                list.Add("a");
-            }
+            // ArrayList list = new ArrayList();
+            // for (int i = 0; i < 10000000; i++) {
+            //     list.Add("a");
+            // }
             
+            TestObject testObject = new TestObject("Testy");
+
             Console.WriteLine("took=" + (DateTime.UtcNow - before).TotalMilliseconds + "ms");
         }
     }
